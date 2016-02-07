db69ee78850773d9820a8b8860ee4d24676cc0ca
alexisAvenel <avenel.alexis@sfr.fr>
2016-02-07 10:50:36 +0100

    Ajout de la colonne avatar dans la table users et mise en place du dashboard admin (25%)

--- resources/views/layouts/master-admin.blade.php
+++ resources/views/layouts/master-admin.blade.php
@@ -18,10 +18,36 @@
                 </a>
             </div>
             <ul id="nav-mobile" class="side-nav fixed" style="width: 240px;">
-                <li class="logo">
-                    <a id="logo-container" href="http://materializecss.com/" class="brand-logo">
-                        Alexis-A
-                    </a>
+                <li class="user-details cyan darken-2">
+                    <div class="row">
+                        <div class="col col s4 m4 l4">
+                            <img src="images/avatar.jpg" alt="" class="circle responsive-img valign profile-image">
+                        </div>
+                        <div class="col col s8 m8 l8">
+                            <a class="btn-flat dropdown-button waves-effect waves-light white-text profile-btn" href="#" data-activates="profile-dropdown">
+                                John Doe<i class="mdi-navigation-arrow-drop-down right"></i>
+                            </a>
+                            <ul id="profile-dropdown" class="dropdown-content">
+                                <li>
+                                    <a href="#"><i class="mdi-action-face-unlock"></i> Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="mdi-action-settings"></i> Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="mdi-communication-live-help"></i> Help</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#"><i class="mdi-action-lock-outline"></i> Lock</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="mdi-hardware-keyboard-tab"></i> Logout</a>
+                                </li>
+                            </ul>
+                            <p class="user-roal">Administrator</p>
+                        </div>
+                    </div>
                 </li>
                 <li class="search">
                     <div class="search-wrapper card">
@@ -29,65 +55,44 @@
                         <div class="search-results"></div>
                     </div>
                 </li>
-                <li class="bold"><a href="about.html" class="waves-effect waves-teal">À propos</a></li>
-                <li class="bold"><a href="getting-started.html" class="waves-effect waves-teal">Commencer</a></li>
+                <li class="bold"><a href="about.html" class="waves-effect waves-orange">À propos</a></li>
+                <li class="bold"><a href="getting-started.html" class="waves-effect waves-orange">Commencer</a></li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header  waves-effect waves-teal">CSS</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-orange">CSS</a>
                             <div class="collapsible-body" style="">
                                 <ul>
                                     <li><a href="color.html">Couleur</a></li>
                                     <li><a href="grid.html">Grid</a></li>
                                     <li><a href="helpers.html">Helpers</a></li>
                                     <li><a href="media-css.html">Media</a></li>
-                                    <li><a href="sass.html">Sass</a></li>
-                                    <li><a href="shadow.html">Shadow</a></li>
-                                    <li><a href="table.html">Table</a></li>
-                                    <li><a href="typography.html">Typography</a></li>
                                 </ul>
                             </div>
                         </li>
-                        <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Components</a>
+                        <li class="bold"><a class="collapsible-header  waves-effect waves-orange">Components</a>
                             <div class="collapsible-body" style="">
                                 <ul>
                                     <li><a href="badges.html">Badges</a></li>
                                     <li><a href="buttons.html">Buttons</a></li>
                                     <li><a href="breadcrumbs.html">Breadcrumbs</a></li>
                                     <li><a href="cards.html">Cards</a></li>
-                                    <li><a href="chips.html">Chips</a></li>
-                                    <li><a href="collections.html">Collections</a></li>
-                                    <li><a href="footer.html">Footer</a></li>
-                                    <li><a href="forms.html">Forms</a></li>
-                                    <li><a href="icons.html">Icons</a></li>
-                                    <li><a href="navbar.html">Navbar</a></li>
-                                    <li><a href="pagination.html">Pagination</a></li>
-                                    <li><a href="preloader.html">Preloader</a></li>
                                 </ul>
                             </div>
                         </li>
-                        <li class="bold"><a class="collapsible-header  waves-effect waves-teal">JavaScript</a>
+                        <li class="bold"><a class="collapsible-header  waves-effect waves-orange">JavaScript</a>
                             <div class="collapsible-body" style="">
                                 <ul>
                                     <li><a href="collapsible.html">Collapsible</a></li>
                                     <li><a href="dialogs.html">Dialogs</a></li>
                                     <li><a href="dropdown.html">Dropdown</a></li>
                                     <li><a href="media.html">Media</a></li>
-                                    <li><a href="modals.html">Modals</a></li>
-                                    <li><a href="parallax.html">Parallax</a></li>
-                                    <li><a href="pushpin.html">Pushpin</a></li>
-                                    <li><a href="scrollfire.html">ScrollFire</a></li>
-                                    <li><a href="scrollspy.html">Scrollspy</a></li>
-                                    <li><a href="side-nav.html">SideNav</a></li>
-                                    <li><a href="tabs.html">Tabs</a></li>
-                                    <li><a href="transitions.html">Transitions</a></li>
-                                    <li><a href="waves.html">Waves</a></li>
                                 </ul>
                             </div>
                         </li>
                     </ul>
                 </li>
-                <li class="bold"><a href="http://materializecss.com/mobile.html" class="waves-effect waves-teal">Mobile</a></li>
-                <li class="bold"><a href="showcase.html" class="waves-effect waves-teal">Showcase</a></li>
+                <li class="bold"><a href="http://materializecss.com/mobile.html" class="waves-effect waves-orange">Mobile</a></li>
+                <li class="bold"><a href="showcase.html" class="waves-effect waves-orange">Showcase</a></li>
             </ul>
         </header>
         <main>
